332a14c6d6e627dbcc9c8450524786f1b67acd74
Peter Szombati <szombatipeti7@gmail.com>
2019-06-07 19:13:40 +0200

    fix error in test

--- src/test/getLogin.ts
+++ src/test/getLogin.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import {XAPIConfig} from "..";
 
 export function getLogin(file: string): {accountId: string, password: string, type: string} {
-	const login = path.join(process.cwd(), file);
+	const login = path.join(process.cwd(), "sensitive", file);
 	if (!fs.existsSync(login)) {
 		throw `${login} is not exists.`
 	}
@@ -14,7 +14,7 @@ export function getLogin(file: string): {accountId: string, password: string, ty
 	||	typeof (type) !== "string") {
 		throw `${login} is not valid`
 	}
-	if (!["real", "demo"].every(x => x === type.toLowerCase())) {
+	if (["real", "demo"].every(x => x !== type.toLowerCase())) {
 		throw `${login} not contains valid type (it should be 'real' or 'demo')`;
 	}
 	return { accountId, password, type };
