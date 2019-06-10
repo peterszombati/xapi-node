3cf04ed7bdea0e119b0a97140d0ead843e4f6127
Peter Szombati <szombatipeti7@gmail.com>
2019-06-11 01:42:50 +0200

    changes in parseLogin.ts

--- src/modules/parseLogin.ts
+++ src/modules/parseLogin.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
-import * as path from 'path';
-import {XAPIConfig} from "../index";
+import {XAPIConfig} from "..";
 
-export function parseLogin(loginJsonFile: string): {accountId: string, password: string, type: string} {
+export function parseLogin(loginJsonFile: string): { accountId: string, password: string, type: string } {
 	if (!fs.existsSync(loginJsonFile)) {
 		throw `${loginJsonFile} is not exists.`
 	}
@@ -10,13 +9,20 @@ export function parseLogin(loginJsonFile: string): {accountId: string, password:
 	try {
 		json = JSON.parse(fs.readFileSync(loginJsonFile).toString().trim())
 	} catch (e) {
-		throw `${loginJsonFile} is not valid json file`;
+		throw `${loginJsonFile} is not valid json file\n${e}`;
+	}
+	if (typeof(json) !== 'object') {
+		throw `${loginJsonFile} is not valid json file\n${typeof(json)}`;
 	}
 
-	const {accountId, password, type}: XAPIConfig = json;
+	const {accountId, password, type, rateLimit, host, appName}: XAPIConfig = json;
 	if (typeof (accountId) !== "string"
 	||	typeof (password) !== "string"
-	||	typeof (type) !== "string") {
+	||	typeof (type) !== "string"
+	||	!['undefined', 'number'].includes(typeof (rateLimit))
+	||	!['undefined', 'string'].includes(typeof (host))
+	||	!['undefined', 'string'].includes(typeof (appName))
+	||	Object.keys(json).length > 6) {
 		throw `${loginJsonFile} is not valid`
 	}
 	if (["real", "demo"].every(x => x !== type.toLowerCase())) {
