38590143a4a011aaa545c447e48c86aaf59adafe
Peter Szombati <szombatipeti7@gmail.com>
2019-07-30 17:01:14 +0200

    add new error code

--- src/enum/errorCode.ts
+++ src/enum/errorCode.ts
@@ -4,5 +4,6 @@ export enum errorCode {
 	XAPINODE_2 = 'XAPINODE_2',
 	XAPINODE_3 = 'XAPINODE_3', // Transaction timeout (60s)
 	XAPINODE_BE103 = 'XAPINODE_BE103',
-	BE005 = 'BE005' // "userPasswordCheck: Invalid login or password"
+	BE005 = 'BE005', // "userPasswordCheck: Invalid login or password"
+	BE118 = 'BE118' // User already logged
 }
