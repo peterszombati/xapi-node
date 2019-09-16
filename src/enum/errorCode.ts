35638e2fd877e4f7f2dd97e078cbde7ea3b83f1f
Peter Szombati <szombatipeti7@gmail.com>
2019-09-16 22:54:53 +0200

    add errorCode

--- src/enum/errorCode.ts
+++ src/enum/errorCode.ts
@@ -1,7 +1,7 @@
 export enum errorCode {
 	XAPINODE_0 = 'XAPINODE_0', // Each command invocation should not contain more than 1kB of data.
 	XAPINODE_1 = 'XAPINODE_1', // WebSocket closed
-
+	XAPINODE_2 = 'XAPINODE_2', // messageQueues exceeded 150 size limit
 	XAPINODE_3 = 'XAPINODE_3', // Transaction timeout (60s)
 	XAPINODE_BE103 = 'XAPINODE_BE103', // User is not logged
 	BE005 = 'BE005', // "userPasswordCheck: Invalid login or password"
