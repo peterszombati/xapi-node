a79250846e6cd786bd6e1e6f359e456146dc1d0b
Peter Szombati <szombatipeti7@gmail.com>
2019-12-23 02:11:59 +0100

    add protocol-docs

--- src/test/messageQueuStressTest.ts
+++ src/test/messageQueuStressTest.ts
@@ -7,7 +7,7 @@
  */
 
 import XAPI from '../core/XAPI';
-import {parseLogin} from '..';
+import {CMD_FIELD, parseLogin, TYPE_FIELD} from '..';
 import Logger4 from 'logger4';
 import * as path from 'path';
 import {Time} from '../modules/Time';
@@ -45,6 +45,26 @@ export function messageQueuStressTest(jsonPath: string) {
                     process.exit(0);
                 }
             }, 40000);
+
+        });
+
+        x.Socket.send.tradeTransaction({
+            cmd: CMD_FIELD.BUY_LIMIT,
+            customComment: null,
+            expiration: new Date().getTime() + 60000 * 60 * 24 * 365,
+            offset: 0,
+            order: 0,
+            price: 21900,
+            sl: 0,
+            symbol: 'US30',
+            tp: 26500,
+            type: TYPE_FIELD.OPEN,
+            volume: 0.2
+        }).then(({returnData}) => {
+            console.log('Success ' + returnData.order);
+        }).catch(e => {
+            console.error('Failed');
+            console.error(e);
         });
 
         x.Socket.listen.getVersion((returnData) => {
