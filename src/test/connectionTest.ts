5e37439a76643459e570d511833a7735ace66807
Peter Szombati <szombatipeti7@gmail.com>
2019-12-25 21:11:11 +0100

    add new test

---
+++ src/test/connectionTest.ts
@@ -0,0 +1,41 @@
+/* sensitive/sensitive.json
+{
+	"accountId": "",
+	"password": "",
+	"type": "real"
+}
+ */
+
+import XAPI from '../core/XAPI';
+import {ConnectionStatus, parseLogin} from '..';
+import Logger4 from 'logger4';
+import * as path from 'path';
+import {Time} from '..';
+
+process
+    .on('unhandledRejection', (reason, p) => {
+        console.error(reason, 'Unhandled Rejection at Promise', p);
+    })
+    .on('uncaughtException', err => {
+        console.error(err, 'Uncaught Exception thrown');
+        process.exit(1);
+    });
+
+export function connectionTest(jsonPath: string) {
+    try {
+        const login = parseLogin(jsonPath);
+        const logger = new Logger4({path: path.join(process.cwd(), 'logs', 'xapi'), maxDirectorySizeInMB: null});
+        const x = new XAPI({...login, logger});
+        x.connect();
+        x.onReady(() => {
+            console.log('Connection is ready');
+        });
+        x.onConnectionChange(status => {
+            console.log(ConnectionStatus[status]);
+        });
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+
+}
