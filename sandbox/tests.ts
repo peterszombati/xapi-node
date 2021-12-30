80c9547ce9f3ef1d240af9612b25ef88cba55d8b
Peter Szombati <szombatipeti7@gmail.com>
2021-12-31 00:16:25 +0100

    create test

---
+++ sandbox/tests.ts
@@ -0,0 +1,19 @@
+import {parseLoginFile} from "./parseLoginFile"
+import * as path from "path"
+import XAPI from "../src"
+import {Logger4V2} from "logger4"
+import {tradeTest} from "./test/tradeTest"
+
+async function init(): Promise<XAPI> {
+    const login = parseLoginFile(path.join(process.cwd(), 'sensitive', 'sensitive-demo-login.json'))
+    const x = new XAPI({...login, logger: new Logger4V2()})
+    await x.connect()
+    return x
+}
+
+describe('tests', () => {
+    it('stcTest', async () => {
+        const x = await init()
+        await tradeTest(x)
+    })
+})
