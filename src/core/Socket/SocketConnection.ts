d5aabbf0161ab136b7191815bbd0a19127639699
Peter Szombati <szombatipeti7@gmail.com>
2022-10-15 00:11:37 +0200

    fix if statement in SocketConnection.ts

--- src/core/Socket/SocketConnection.ts
+++ src/core/Socket/SocketConnection.ts
@@ -155,12 +155,12 @@ export class SocketConnection extends Queue {
         + ', accountType = ' + this.XAPI.accountType
         + ') Reason:' + JSON.stringify(e))
 
-      if (retries > 0 && e.reason.code !== errorCode.XAPINODE_1 && e.reason.code !== errorCode.BE005) {
+      if (retries > 0 && (!e.reason || (e.reason.code !== errorCode.XAPINODE_1 && e.reason.code !== errorCode.BE005))) {
         this.loginTimeout.setTimeout(() => {
           this.XAPI.logger.print('debug', `${new Date().toISOString()}: Try to login (retries = ${retries})`)
           this.tryLogin(retries - 1)
         }, 500)
-      } else if (e.reason.code === errorCode.BE005) {
+      } else if (e.reason && e.reason.code === errorCode.BE005) {
         this.XAPI.logger.print('debug', `${new Date().toISOString()}: Disconnect from stream and socket (reason = 'login error code is ${e.reason.code}')`)
         this.XAPI.disconnect()
       }
