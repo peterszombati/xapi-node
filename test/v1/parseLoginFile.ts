ca9df5b998c5c4e1df618f39a4e33fbb891a2bb3
Peter Szombati <szombatipeti7@gmail.com>
2023-10-24 00:56:02 +0200

    remove v1 test files

--- test/v1/parseLoginFile.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs'
-import { parseJsonLogin, XAPIConfig } from '../../src'
-
-export function parseLoginFile(loginJsonFile: string): XAPIConfig {
-  if (!fs.existsSync(loginJsonFile)) {
-    throw `${loginJsonFile} is not exists.`
-  }
-  return parseJsonLogin(fs.readFileSync(loginJsonFile).toString())
-}
