5ca62c2ac1f8b006b2f3236658a1dc29066a0fdb
Peter Szombati <szombatipeti7@gmail.com>
2023-10-27 06:04:06 +0200

    refactor getObjectChanges.ts

--- src/v2/utils/getObjectChanges.ts
+++ src/v2/utils/getObjectChanges.ts
@@ -1,9 +1,11 @@
 export function getObjectChanges(from: Record<string, any>, to: Record<string, any>) {
-    const obj: Record<string, any> = {}
-    Object.keys(from)
-        .filter(key => from[key] !== to[key])
-        .forEach(key => {
-            obj[key] = to[key]
-        })
-    return obj
+  const obj: Record<string, any> = {}
+
+  for (const [key, value] of Object.entries(from)) {
+    if (value !== to[key]) {
+      obj[key] = to[key]
+    }
+  }
+
+  return obj
 }
