552e504bd9b50d5d0ab70523e108a28d338817fb
Peter Szombati <szombatipeti7@gmail.com>
2023-11-01 23:15:49 +0100

    tests.ts

--- test/v2/tests.ts
+++ test/v2/tests.ts
@@ -6,6 +6,12 @@ import { connectionTest } from './test/connectionTest'
 //import { subscribeTest } from './test/subscribeTest'
 //import { getCandlesTest } from './test/getCandlesTest'
 import {XAPI} from "../../src/v2/core/XAPI"
+import {tradeTest} from "./test/tradeTest";
+import {subscribeTest} from "./test/subscribeTest";
+import {getCandlesTest} from "./test/getCandlesTest";
+import {messageQueuStressTest} from "./test/messageQueuStressTest";
+import { Logger } from '../../src';
+import { Counter } from '../../src';
 
 const jsonPath = path.join(process.cwd(), 'sensitive', 'sensitive-demo-login.json')
 /* sensitive/sensitive-demo-login.json
@@ -16,10 +22,34 @@ const jsonPath = path.join(process.cwd(), 'sensitive', 'sensitive-demo-login.jso
 }
  */
 
+let x: XAPI | null = null
 async function init(): Promise<XAPI> {
-  const login = parseLoginFile(jsonPath)
-  const x = new XAPI({ ...login })
-  await x.connect()
+  if (!x || Object.keys(x.Socket.connections).length === 0) {
+    const login = parseLoginFile(jsonPath)
+    const l = new Logger()
+    l.on({
+      type: 'debug',
+      callback: data => console.log(data)
+    })
+    l.on({
+      type: 'transaction',
+      callback: data => console.log(data)
+    })
+    l.on({
+      type: 'error',
+      callback: data => console.log(data)
+    })
+    l.on({
+      type: 'info',
+      callback: data => console.log(data)
+    })
+    const c = new Counter()
+    c.on({
+      callback: ({key,time,count}) => console.log(time.get().toISOString()+':'+key.join(':')+':'+count)
+    })
+    x = new XAPI({...login},l,c)
+    await x.connect()
+  }
   return x
 }
 
@@ -28,7 +58,7 @@ describe('tests', () => {
     const x = await init()
     await connectionTest(x)
   })
-/*
+
   it('messageQueuStressTest', async function () {
     this.timeout(8000)
     const x = await init()
@@ -56,5 +86,5 @@ describe('tests', () => {
     const x = await init()
     await tradeTest(x)
     x.disconnect()
-  })*/
+  })
 })
