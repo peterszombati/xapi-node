aefe1660eda20f67064b2bdfb4928f239d45ea62
Peter Szombati <szombatipeti7@gmail.com>
2023-11-02 04:20:49 +0100

    improve tradeTest.ts

--- test/v2/test/tradeTest.ts
+++ test/v2/test/tradeTest.ts
@@ -7,17 +7,20 @@ export function tradeTest(x: XAPI): Promise<void> {
   return new Promise(async (resolve, reject) => {
     try {
       await x.Stream.subscribe.getTrades()
-      await x.trading.buy({
+      await x.Stream.subscribe.getTradeStatus()
+      const r = await x.trading.buy({
         symbol,
         volume,
         limit: 0.0987
       }).transactionStatus
-      const position = x.trading.limitPositions?.find(i => i.symbol === symbol && i.volume >= volume && i.open_price === 0.0987)
+      //@ts-ignore
+      const position = x.trading.limitPositions?.find(i => i.order === r.order && i.symbol === symbol && i.volume >= volume && i.open_price === 0.0987)
       if (!position) {
         throw new Error('position not found;' + x.trading.openPositions)
       }
-      await x.trading.close({ order: position.order })
-      const position1 = x.trading.limitPositions?.find(i => i.symbol === symbol && i.volume >= volume && i.open_price === 0.0987)
+      await x.trading.close({ order: position.position }).transactionStatus
+      //@ts-ignore
+      const position1 = x.trading.limitPositions?.find(i => i.position === position.position && i.symbol === symbol && i.volume >= volume && i.open_price === 0.0987)
       if (position1) {
         throw new Error('position found after close;' + JSON.stringify(x.trading.openPositions?.map(i => i.valueOf())))
       }
